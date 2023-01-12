3d7a0593c47fc6941f6a120ad5f0091051e059dc
Shuaicong Wu <niklaus.fangtasia@gmail.com>
2023-01-13 00:32:45 +0100

    update About.js

--- mememuc-template/react-frontend/src/pages/About.js
+++ mememuc-template/react-frontend/src/pages/About.js
@@ -4,7 +4,12 @@ function About() {
     return (
         <div>
             <h1>About</h1>
-            <p>here to describe how to use the app</p>
+            <p>Here is a group project called "Meme Generator" for the Online Multimedia Lecture in the winter semester 2022/23 at LMU Munich.
+                It is a website similar to <a href={"https://imgflip.com/"}>imgflip</a>.</p>
+            <p>Team Members (Group 002):<br/>
+                Shuaicong Wu<br/>
+                Shuning Zhang
+            </p>
         </div>
     );
 }
