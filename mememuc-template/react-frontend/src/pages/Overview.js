22bd0ba205ca7f29b4809ceae04c59aa620e2dce
Shuaicong Wu <niklaus.fangtasia@gmail.com>
2023-01-30 18:40:34 +0100

    sort by date and title on Overview

--- mememuc-template/react-frontend/src/pages/Overview.js
+++ mememuc-template/react-frontend/src/pages/Overview.js
@@ -2,9 +2,11 @@
  * References:
  * https://react-bootstrap.github.io/components/navbar/
  * https://www.youtube.com/watch?v=SLfhMt5OUPI
- * https://www.youtube.com/watch?v=bBUOMy6Tugw (Potential)
+ * https://www.youtube.com/watch?v=bBUOMy6Tugw (Not used yet)
  * https://getbootstrap.com/docs/4.0/layout/media-object/#example
  * https://stackoverflow.com/questions/8919682/remove-all-styling-formatting-from-hyperlinks
+ * Sort by Alphabet:
+ * https://stackoverflow.com/questions/19259233/sorting-json-by-specific-element-alphabetically
  * */
 
 import React, {useState, useEffect} from "react";
@@ -14,8 +16,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "../styles/overview.css";
 
+// TODO (Bug): When we refresh the page '/#sort=latest', the URL doesn't change, but the content will be the same as the home page.
 function Overview() {
     const [data, setData] = useState([]);
+    const [sort, setSort] = useState('default');
 
     useEffect(() => {
         fetch(" http://localhost:3002/memes/get-memes")
@@ -28,6 +32,18 @@ function Overview() {
 
     const handleMeme = () => {
         window.localStorage.setItem("memeFrom", "Overview");
+    }
+
+    const handleDefault = () => {
+        setSort('default');
+    }
+
+    const handleLatest = () => {
+       setSort('latest');
+    }
+
+    const handleTitle = () => {
+        setSort('title');
     }
 
     const Meme = ({meme}) => {
@@ -50,8 +66,9 @@ function Overview() {
             <Navbar className="top">
                 <Container>
                     <Nav className="me-auto">
-                        <Nav.Link href="/#sort=latest">New</Nav.Link>
-                        <Nav.Link href="/#sort=title">Title</Nav.Link>
+                        <Nav.Link href="/" onClick={handleDefault}>Default</Nav.Link>
+                        <Nav.Link href="/#sort=latest" onClick={handleLatest}>New</Nav.Link>
+                        <Nav.Link href="/#sort=title" onClick={handleTitle}>Title</Nav.Link>
                         <NavDropdown
                             title="Sort by"
                             menuVariant="light">
@@ -62,8 +79,23 @@ function Overview() {
                 </Container>
             </Navbar>
 
-            {data && data.map((meme) =>
-                meme.permission == "public" &&
+            {sort === "default" && data && data.map(meme => meme.permission === "public" &&
+                <Meme key={meme.url}
+                      meme={meme}
+                />
+            )}
+
+            {sort === "latest" && data && data.sort(function (a, b) {
+                return a.date < b.date ? 1 : a.date > b.date ? -1 : 0;
+            }) && data.map(meme => meme.permission === "public" &&
+                <Meme key={meme.url}
+                      meme={meme}
+                />
+            )}
+
+            {sort === "title" && data && data.sort( function (a, b) {
+                return a.title < b.title ? -1 : a.title > b.title ? 1 : 0;
+            }) && data.map(meme => meme.permission === "public" &&
                 <Meme key={meme.url}
                       meme={meme}
                 />
